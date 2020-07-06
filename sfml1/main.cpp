91522927230e481e33aaa88816c7dbc08c4f9400
Gleb <skryabin.gleb.d@gmail.com>
2020-07-06 22:48:07 +0300

    upd1classline

--- sfml1/main.cpp
+++ sfml1/main.cpp
@@ -25,14 +25,11 @@
 #include <cmath>
 
 
-struct point { double x, y, xn, yn, a; };
 double dabs(double x){ return x > 0 ? x : - x; }
-double dist(point a, point b){ return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2)); }
 double dist(sf::Vector2i a, sf::Vector2i b){ return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2)); }
 const int height = 1400, width = 2100;
 const int xcenter = width/2, ycenter = height/2;
 const bool debug = false;
-//const int n = 15;
 
 double get_angel(double x, double y){
     y *= -1;
@@ -45,37 +42,141 @@ double get_angel(double x, double y){
     }
 }
 
-void sin_line(sf::VertexArray *line, int n){
-    int scale = 120, thickness = 30, htn = thickness/2;
-    double xl = -5.0, xr = 5.0, step = (xr - xl)/n;
-    //sf::VertexArray line(sf::TriangleStrip, n*2);
-    point zln[n];
-    for (int i = 0; i < n; i++){
-        zln[i].x = xl + i*step;
-        zln[i].y = sin(zln[i].x);
+
+class Line{
+public:
+    struct Point { double x, y, xn, yn, a; } * zln;
+    struct Mouse { sf::Vector2i lastpos, pos; } mouse;
+    struct Settings { int n, thickness, htn, min_distance; } settings;
+    sf::VertexArray skin;
+    
+    Line(int n, int thickness, int mindist, sf::Vector2i mouse_pos){
+        settings.n = n;
+        settings.thickness = thickness;
+        settings.htn = thickness/2;
+        settings.min_distance = mindist;
+        mouse.lastpos = mouse_pos;
+        zln = (Point *)malloc(n * sizeof(Point));
+        skin.setPrimitiveType(sf::TriangleStrip);
+        skin.resize(n*2);
     }
     
-    for (int i = 1; i < n - 1; i++){
-        double xi = zln[i].x, yi = zln[i].y;
-        double xm = xi - zln[i - 1].x, ym = yi - zln[i - 1].y;
-        double xp = xi - zln[i + 1].x, yp = yi - zln[i + 1].y;
-        double lm = sqrt(xm*xm+ym*ym), lp = sqrt(xp*xp+yp*yp);
-        double xn = xm / lm + xp / lp, yn = ym / lm + yp / lp;
-        double ln = sqrt(xn*xn+yn*yn);
-        zln[i].xn = dabs(xn / ln);
-        zln[i].yn = dabs(yn / ln);
+    void reinit(int new_n, int new_thickness, int new_mindist){
+        settings.n = new_n;
+        settings.thickness = new_thickness;
+        settings.htn = new_thickness/2;
+        settings.min_distance = new_mindist;
+        zln = (Point *)realloc(zln, new_n * sizeof(Point));
+        skin.resize(new_n*2);
     }
-    zln[0].xn = 0;
-    zln[0].yn = 1;
-    zln[n - 1].xn = 0;
-    zln[n - 1].yn = 1;
     
-    for (int i = 0; i < n; i++){
-        int wx = int(xcenter + zln[i].x*scale), wy = int(ycenter + zln[i].y*scale);
-        (*line)[i*2].position = sf::Vector2f(wx - zln[i].xn * htn, wy - zln[i].yn * htn);
-        (*line)[i*2 + 1].position = sf::Vector2f(wx + zln[i].xn * htn, wy + zln[i].yn * htn);
+    void update_line(sf::Vector2i mouse_new_pos){
+        //pos = sf::Mouse::getPosition(window);
+        mouse.pos = mouse_new_pos;
+        if (dist(mouse.lastpos, mouse.pos) >= settings.min_distance){
+            for (int i = settings.n - 1; i > 0; i--){
+                zln[i] = zln[i - 1];
+            }
+            zln[0].x = mouse.pos.x;
+            zln[0].y = mouse.pos.y;
+            mouse.lastpos = mouse.pos;
+            
+            //zln[1]
+            double x1 = zln[1].x, y1 = zln[1].y;
+            double xm = x1 - zln[0].x, ym = y1 - zln[0].y;
+            double xp = x1 - zln[2].x, yp = y1 - zln[2].y;
+            double lm = sqrt(xm*xm+ym*ym), lp = sqrt(xp*xp+yp*yp);
+            xm /= lm;
+            ym /= lm;
+            xp /= lp;
+            yp /= lp;
+            double xn = xm + xp, yn = ym + yp;
+            if (xn != 0 && yn != 0){
+                double ln = sqrt(xn*xn+yn*yn);
+                xn /= ln;
+                yn /= ln;
+                double am = get_angel(xm, ym), an = get_angel(xn, yn);
+                double alpha = an - am;
+                alpha += alpha >= 0 ? 0 : 2*M_PI;
+                int sgn = alpha <= M_PI ? 1 : -1;
+                zln[1].xn = sgn * xn;
+                zln[1].yn = sgn * yn;
+                zln[1].a  = alpha;
+            } else {
+                xn = xm - xp;
+                yn = ym - yp;
+                double ln = sqrt(xn*xn+yn*yn);
+                zln[1].xn =  yn / ln;
+                zln[1].yn = -xn / ln;
+                zln[1].a  = M_PI / 2;
+            }
+            //zln[0]
+            xp = zln[0].x - zln[1].x;
+            yp = zln[0].y - zln[1].y;
+            lp = sqrt(xp*xp+yp*yp);
+            zln[0].xn = -yp / lp;
+            zln[0].yn =  xp / lp;
+            zln[0].a  = M_PI / 2;
+            //zln[n - 1]
+            xm = zln[settings.n - 1].x - zln[settings.n - 2].x;
+            ym = zln[settings.n - 1].y - zln[settings.n - 2].y;
+            lm = sqrt(xm*xm+ym*ym);
+            zln[settings.n - 1].xn =  ym / lm;
+            zln[settings.n - 1].yn = -xm / lm;
+            zln[settings.n - 1].a  = M_PI / 2;
+            
+            for (int i = 0; i < settings.n; i++){
+                int wx = int(zln[i].x), wy = int(zln[i].y);
+                //int wxn = zln[i].xn * settings.htn, wyn = zln[i].yn * settings.htn;
+                skin[i*2].color = sf::Color::Magenta;
+                skin[i*2].position = sf::Vector2f(wx - zln[i].xn * settings.htn, wy - zln[i].yn * settings.htn);
+                skin[i*2 + 1].position = sf::Vector2f(wx + zln[i].xn * settings.htn, wy + zln[i].yn * settings.htn);
+            }
+        }
     }
-}
+    
+    
+    
+
+    
+    void sin_line(sf::VertexArray *line, int n){
+        int scale = 120, thickness = 30, htn = thickness/2;
+        double xl = -5.0, xr = 5.0, step = (xr - xl)/n;
+        //sf::VertexArray line(sf::TriangleStrip, n*2);
+        Point zln[n];
+        for (int i = 0; i < n; i++){
+            zln[i].x = xl + i*step;
+            zln[i].y = sin(zln[i].x);
+        }
+        
+        for (int i = 1; i < n - 1; i++){
+            double xi = zln[i].x, yi = zln[i].y;
+            double xm = xi - zln[i - 1].x, ym = yi - zln[i - 1].y;
+            double xp = xi - zln[i + 1].x, yp = yi - zln[i + 1].y;
+            double lm = sqrt(xm*xm+ym*ym), lp = sqrt(xp*xp+yp*yp);
+            double xn = xm / lm + xp / lp, yn = ym / lm + yp / lp;
+            double ln = sqrt(xn*xn+yn*yn);
+            zln[i].xn = dabs(xn / ln);
+            zln[i].yn = dabs(yn / ln);
+        }
+        zln[0].xn = 0;
+        zln[0].yn = 1;
+        zln[n - 1].xn = 0;
+        zln[n - 1].yn = 1;
+        
+        for (int i = 0; i < n; i++){
+            int wx = int(xcenter + zln[i].x*scale), wy = int(ycenter + zln[i].y*scale);
+            (*line)[i*2].position = sf::Vector2f(wx - zln[i].xn * htn, wy - zln[i].yn * htn);
+            (*line)[i*2 + 1].position = sf::Vector2f(wx + zln[i].xn * htn, wy + zln[i].yn * htn);
+        }
+    }
+    
+    ~Line(){
+        free(zln);
+    }
+};
+
+
 
 
 
@@ -83,11 +184,17 @@ int main(){
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
     sf::RenderWindow window(sf::VideoMode(width, height), "Thick Line", sf::Style::Default, settings);
-    int n = 50, thickness = 24, htn = thickness/2, mindist = 20, iteration = 0;
-    point zln[n];
+    int n = 20, thickness = 24, htn = thickness/2, mindist = 20, iteration = 0;
+    
+    
+    
+    //Line line2(20, 24, 20);
+    
+    
+    Line::Point zln[n];
     sf::VertexArray line(sf::TriangleStrip, n*2);
     //sf::VertexArray line(sf::Triangles, (n - 1)*6);
-    sf::Vector2i pos = sf::Mouse::getPosition(window), nextpos;
+    sf::Vector2i lastpos = sf::Mouse::getPosition(window), pos;
     sf::Vertex axes[] = {
         sf::Vertex(sf::Vector2f(0, ycenter)), sf::Vertex(sf::Vector2f(width, ycenter)),
         sf::Vertex(sf::Vector2f(xcenter, 0)), sf::Vertex(sf::Vector2f(xcenter, height)),
@@ -118,14 +225,14 @@ int main(){
         iteration ++;
         window.clear();
         
-        nextpos = sf::Mouse::getPosition(window);
-        if (dist(pos, nextpos) >= mindist){
+        pos = sf::Mouse::getPosition(window);
+        if (dist(lastpos, pos) >= mindist){
             for (int i = n - 1; i > 0; i--){
                 zln[i] = zln[i - 1];
             }
-            zln[0].x = nextpos.x;
-            zln[0].y = nextpos.y;
-            pos = nextpos;
+            zln[0].x = pos.x;
+            zln[0].y = pos.y;
+            lastpos = pos;
             
             /*for (int i = 1; i < n - 1; i++){
                 double xi = zln[i].x, yi = zln[i].y;
